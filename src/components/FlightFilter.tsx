a15160f33a659831ecf71f41c4c6557920cf0b12
kutaui <kutaybekleric@gmail.com>
2023-08-19 23:31:15 +0300

    fix: type errors

--- src/components/FlightFilter.tsx
+++ src/components/FlightFilter.tsx
@@ -11,7 +11,6 @@ import {convertValueToTime} from "@/lib/ConvertValueToTime.ts";
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
